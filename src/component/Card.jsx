f634c563d51396b8ed9db837d5a7b611d2e831a6
gangadhar195 <gangadhars9545@gmail.com>
2024-08-04 15:07:42 +0530

    class add

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -5,7 +5,7 @@ function Card(props){
     return(
      <div className="term">
        <dt>
-        <span className="emoji" role="img" aria-label="Tense Biceps">
+        <span className="emoji img-fluid" role="img" aria-label="Tense Biceps" >
           {props.emoji}
         </span>
         <span>{props.name}</span>
